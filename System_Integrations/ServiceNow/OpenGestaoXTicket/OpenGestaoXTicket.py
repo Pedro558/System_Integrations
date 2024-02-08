bda2ba463e0cc281c3e485576d6f17ebe251f288
Lucas Buchaul <lucas.buchaul@piemonteholding.com>
2024-02-08 09:41:06 -0300

    Test: Debug networks ticket posting

--- System_Integrations/ServiceNow/OpenGestaoXTicket/OpenGestaoXTicket.py
+++ System_Integrations/ServiceNow/OpenGestaoXTicket/OpenGestaoXTicket.py
@@ -399,6 +399,8 @@ def process_data(url, ritm_list):
                     descricao += f"\nTelefone 2: {valueMobilePhone}"
                     descricao += descriptionBuilder(variables, aConfig)
                     descricao += get_multi_row_question_answer(ritm['sys_id'], ritm['cat_item.name'])
+                    
+            print(descricao+"\n")
 
         else:
             continue
@@ -430,12 +432,15 @@ def openGestaoXTicket(url, tickets_to_post):
     try:
         results =[]
         for ticket in tickets_to_post:
+            print("\n------------ANTES DO POST------------\n"+ticket["Descricao"]+"\n\n")
             response = requests.post(url, headers=headers, data=json.dumps(ticket['data']))
+            print("\n------------DEPOIS DO POST------------\n"+ticket["Descricao"]+"\n\n")
             if response.status_code == 200 or response.status_code == 201:
                 results.append({
                     "item": ticket,
                     "response": response.__dict__,
                 })  
+                print(results[-1]["item"]["Descricao"])
             else:
                 response.raise_for_status()
 
