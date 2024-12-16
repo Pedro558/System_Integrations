a18185fad4edf3799846c8241dc69e35856eda89
filipeUccelli <160670246+filipeUccelli@users.noreply.github.com>
2024-12-16 17:41:12 -0300

    Feature: New end point multi insert image url

--- System_Integrations/utils/servicenow_api.py
+++ System_Integrations/utils/servicenow_api.py
@@ -165,6 +165,26 @@ def client_monitoring_multi_post(envUrl, data, token, params={}):
             "errorMsg": error # TODO AQUI verificar como extrair mensagem
         }
 
+def client_monitoring_multi_post_img(envUrl, data, token, params={}):
+    url = envUrl + '/api/eldi/client_links_monitoring/multi_insert_image_table'
+    headers = {
+        'Content-Type': 'application/json',
+        'Authorization': 'Bearer '+token,
+    }
+    response = requests.post(url, headers=headers, params=params, data=json.dumps(data))
+    try:
+        response.raise_for_status()
+        return {
+            "response": response,
+            "error": False
+        }
+
+    except Exception as error:
+        return {
+            "response": response,
+            "error": True,
+            "errorMsg": error # TODO AQUI verificar como extrair mensagem
+        }
 
 
 
