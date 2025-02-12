26016208b31962f1fabd4d7d24b53939f223e99c
filipeUccelli <160670246+filipeUccelli@users.noreply.github.com>
2025-02-12 09:32:16 -0300

    Refac: Dashboard cliente env control, venv path and yamls per environment

--- System_Integrations/ServiceNow/Zabbix/DashboardLinks/SyncToSnow.ps1
+++ System_Integrations/ServiceNow/Zabbix/DashboardLinks/SyncToSnow.ps1
@@ -1,10 +1,9 @@
 # Activate the virtual environment
-$venvPath = "C:\python\venv\System_Integrations" # TODO create venv to proxmox
-$activateScript = Join-Path -Path $venvPath -ChildPath "Scripts\Activate.ps1"
+$scriptPath = "C:\rundeck\projects\System_Integrations\ScmImport"
+$activateScript = Join-Path -Path $scriptPath -ChildPath "venv\Scripts\Activate.ps1"
 . $activateScript
 
 # Run your Python script
-$scriptPath = "C:\rundeck\projects\System_Integrations\ScmImport"
 $scriptModule = "System_Integrations.ServiceNow.Zabbix.DashboardLinks.SyncToSnow"
 Set-Location -Path $scriptPath
 python -u -m $scriptModule
