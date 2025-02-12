89e5cd3fbcf6830192d633a8224eb0f0e1777b34
filipeUccelli <160670246+filipeUccelli@users.noreply.github.com>
2025-02-12 09:17:25 -0300

    Refac: GetTicketUpdate env control and venv path

--- System_Integrations/ServiceNow/Gestao_X_Client_Scripts/GetTicketUpdate/GetTicketUpdate.py
+++ System_Integrations/ServiceNow/Gestao_X_Client_Scripts/GetTicketUpdate/GetTicketUpdate.py
@@ -1,20 +1,25 @@
 from System_Integrations.ServiceNow.Strategies.ServiceNow.BaseUpdateStrategy import BaseUpdateStrategy
 from System_Integrations.ServiceNow.Strategies.ServiceNow.ServiceNowTicketProcessingContext import ServiceNowTicketProcessingContext
+from commons.utils.env import only_run_in
 
 #URL produção
 url_gestao_x = "https://csc.everestdigital.com.br/API/"
 url_servicenow_prd = "https://servicenow.eleadigital.com/"
 
+def execute():
+    strat = BaseUpdateStrategy(
+        url_snow=url_servicenow_prd,
+        url_gestao_x=url_gestao_x,
+    )
 
-strat = BaseUpdateStrategy(
-    url_snow=url_servicenow_prd,
-    url_gestao_x=url_gestao_x,
-)
+    ticket_context = ServiceNowTicketProcessingContext(strat)
+    ticket_context.get_auth()
+    ticket_context.fetch_list()
+    ticket_context.processing()
 
-ticket_context = ServiceNowTicketProcessingContext(strat)
-ticket_context.get_auth()
-ticket_context.fetch_list()
-ticket_context.processing()
+    ticket_context.post()
+    ticket_context.show_results()
 
-ticket_context.post()
-ticket_context.show_results()
+if __name__ == "__main__":
+    only_run_in(["Prod"])
+    execute()
