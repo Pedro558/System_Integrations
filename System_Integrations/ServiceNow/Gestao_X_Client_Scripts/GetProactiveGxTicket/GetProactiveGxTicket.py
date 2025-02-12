b0037ceddc768081659120c23c96f3472c86511d
filipeUccelli <160670246+filipeUccelli@users.noreply.github.com>
2025-02-12 09:16:24 -0300

    Refac: GetProactiveGxTicket env control and venv path

--- System_Integrations/ServiceNow/Gestao_X_Client_Scripts/GetProactiveGxTicket/GetProactiveGxTicket.py
+++ System_Integrations/ServiceNow/Gestao_X_Client_Scripts/GetProactiveGxTicket/GetProactiveGxTicket.py
@@ -1,23 +1,28 @@
 from System_Integrations.ServiceNow.Strategies.ServiceNow.ProactiveTicketStrategy import ProactiveTicketStrategy
 from System_Integrations.ServiceNow.Strategies.ServiceNow.ServiceNowTicketProcessingContext import ServiceNowTicketProcessingContext
+from commons.utils.env import only_run_in
 
 #URL produção
 url_gestao_x = "https://csc.everestdigital.com.br/API/"
 url_servicenow_prd = "https://servicenow.eleadigital.com/"
 
+def execute():
+    strat = ProactiveTicketStrategy(
+        url_snow=url_servicenow_prd,
+        url_gestao_x=url_gestao_x,
+    )
 
-strat = ProactiveTicketStrategy(
-    url_snow=url_servicenow_prd,
-    url_gestao_x=url_gestao_x,
-)
+    ticket_context = ServiceNowTicketProcessingContext(strat)
+    ticket_context.get_auth()
+    ticket_context.fetch_list()
+    ticket_context.processing()
 
-ticket_context = ServiceNowTicketProcessingContext(strat)
-ticket_context.get_auth()
-ticket_context.fetch_list()
-ticket_context.processing()
+    ticket_context.post()
+    ticket_context.show_results()
 
-ticket_context.post()
-ticket_context.show_results()
+    ticket_context.post_evidence()
+    ticket_context.show_evidence_results()
 
-ticket_context.post_evidence()
-ticket_context.show_evidence_results()
+if __name__ == "__main__":
+    only_run_in(["Prod"])
+    execute()
