759a23e9ea37b7397dea0dd8d69b7289935fb53d
pedro558 <pedroafonso109@gmail.com>
2025-05-11 07:31:42 -0300

    init: first commit - Trying to understand

--- System_Integrations/ServiceNow/Gestao_X_Client_Scripts/GetTicketUpdate/GetTicketUpdate.ps1
+++
@@ -1,9 +0,0 @@
-# Activate the virtual environment
-$scriptPath = "C:\rundeck\projects\System_Integrations\ScmImport"
-$activateScript = Join-Path -Path $scriptPath -ChildPath "venv\Scripts\Activate.ps1"
-. $activateScript
-
-# Run your Python script
-$scriptModule = "System_Integrations.ServiceNow.Gestao_X_Client_Scripts.GetTicketUpdate.GetTicketUpdate"
-Set-Location -Path $scriptPath
-python -m $scriptModule
